9746b90808565d57ffd484fe9acedac2323ed147
Raj Poudel <raj.poudeel@gmail.com>
2023-07-20 16:12:46 +0545

    smade

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const express  = require('express');
 
 const app = express();
+const port = 4000;
+
+app.get("/",(req,res)=>{
+    
+    res.send("<h1>Hello Home page</h1>")
+});
+app.get("/about",(req,res)=>{
+    
+    res.send("<h1>Hello about page</h1>")
+});
+
+app.get("/service",(req,res)=>{
+    
+    res.send("<h1>Hello service page</h1>")
+});
+
+
+app.listen(port ,() =>{
+    console.log(`Server is Working on port: ${port}`);
+})
