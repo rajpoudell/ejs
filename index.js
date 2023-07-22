1a3ebaaa885c9113e511ba6ba914453c46178fe2
Raj Poudel <raj.poudeel@gmail.com>
2023-07-22 15:40:04 +0545

    init pst

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express  = require('express');
-
+const path = require("path")
 const app = express();
 const port = 4000;
 
 app.get("/",(req,res)=>{
-    
-    res.send("<h1>Hello Home page</h1>")
+    res.sendFile(path.join(__dirname + "/index.html"));
+    console.log(path.join(__dirname + "/index.html"));
 });
-app.get("/about",(req,res)=>{
-    
-    res.send("<h1>Hello about page</h1>")
+
+
+app.post("/nice/get",(req,res) => {
+    res.send("<h1>hello server</h1>");
+    console.log("name");
 });
 
 
