26deb4ad274635100f812f8a35ba5fc5e74e74ef
Raj Poudel <raj.poudeel@gmail.com>
2023-07-23 23:23:06 +0545

    init

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express  = require('express');
-const path = require("path")
+const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 4000;
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"));
-    console.log(path.join(__dirname + "/index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+    console.log(path.join(__dirname, "index.html"));
 });
 
-
-app.post("/nice/get",(req,res) => {
-    res.send("<h1>hello server</h1>");
-    console.log("name");
+app.post("/api/v1/register", (req, res) => {
+    res.send(`<h1>Done Mr.${req.body.name}</h1>`);
+    console.log(req.body.name);
 });
 
-
-app.listen(port ,() =>{
-    console.log(`Server is Working on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
