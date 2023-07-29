ff707273dd9126915b331b321b4d6a1ca4ff9518
Raj Poudel <raj.poudeel@gmail.com>
2023-07-29 17:56:13 +0545

    init

---
+++ userController.js
@@ -0,0 +1,10 @@
+exports.registerUser = (req,res) => {
+    const userName = req.body.name;
+    const userEmail =  req.body.email;
+    const userPassword =  req.body.password;
+
+    res.json({
+        success:true, //send data to user
+    })  ;
+;}
+
